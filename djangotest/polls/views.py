ca6f34031b50f04efe2a937cbcd564529b00125e
dmarkov <markovs.code@gmail.com>
2023-01-24 13:31:10 +0100

    Add tests and replace function views with class based ones

--- djangotest/polls/views.py
+++ djangotest/polls/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
-from .models import Question,  Choice
+from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    all_questions_list = Question.objects.all()
-    context = {
-        'all_questions_list': all_questions_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'all_questions_list'
+
+    def get_queryset(self):
+        return Question.objects.all()
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, id=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Question
 
 
 def vote(request, question_id):
@@ -38,7 +39,3 @@ def vote(request, question_id):
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-
-
